b34f3be77f8a17759887624e3ca5d24b699a58f0
Lucas Lokken <lucas.lokken@gmail.com>
2019-01-20 13:47:17 -0600

    Initial Commit

---
+++ tap-list.php
@@ -0,0 +1,16 @@
+<?php
+
+/** 
+*Plugin Name: Tap List
+*Plugin URI:   https://www.lightandfire.com/plugins/tap-list/
+*Description: Plugin for displaying a list of tap beers on a Wordpress page
+*Version: 1.0.0
+*Author: Lucas Lokken
+*Author URI: https://www.lucaslokken.com
+*License: GPL2
+*License URI: https://www.gnu.org/licenses/gpl-2.0.html
+**/
+
+
+
+?>
